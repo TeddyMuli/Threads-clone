c1a3aa01a50f62b1e0ae442c71c8fbb3d65e85ca
TeddyMuli <tmuli974@gmail.com>
2024-01-23 11:19:36 +0300

    made changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,43 +1,49 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import '../globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
-import LeftSideBar from '@/components/shared/LeftSideBar'
-import Topbar from '@/components/shared/Topbar'
-import RightSideBar from '@/components/shared/RightSideBar'
-import Bottombar from '@/components/shared/Bottombar'
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-const inter = Inter({ subsets: ['latin'] })
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
-export const metadata: Metadata= {
-    title: 'Threads',
-    description: 'A Next.js Meta Threads Application'
-}
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
         <body className={inter.className}>
-          <Topbar/>
+          <Topbar />
+
           <main className='flex flex-row'>
-            <LeftSideBar/>
-
-              <section className='main-container'>
-                <div className='w-full max-w-4xl'>
-                  {children}
-                </div>
-              </section>
-
-            <RightSideBar/>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
           </main>
-          <Bottombar/>
+
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
